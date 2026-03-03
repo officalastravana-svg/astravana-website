ca9611145687a12c9be9ae85441bf674fb0a41fa
officalastravana-svg <officalastravana@gmail.com>
2026-03-03 09:23:50 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,16 +1,11 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["rescue_image"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$targetDir = "uploads/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
 
-// Check file type
-if(in_array($imageFileType, ['jpg','jpeg','png','gif'])){
-    if(move_uploaded_file($_FILES["rescue_image"]["tmp_name"], $target_file)){
-        echo "<script>alert('Photo Uploaded Successfully');window.location.href='index.html';</script>";
-    } else {
-        echo "<script>alert('Upload Failed');window.location.href='index.html';</script>";
-    }
-} else {
-    echo "<script>alert('Invalid File Type');window.location.href='index.html';</script>";
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+header("Location: index.php");
+}else{
+echo "Upload failed.";
 }
 ?>
